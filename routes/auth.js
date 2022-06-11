477656975be4c66960c7d822506d14779497b409
JRam2022 <jasonram778@gmail.com>
2022-06-11 14:03:07 -0700

    Implement login with auth

--- routes/auth.js
+++ routes/auth.js
@@ -5,8 +5,6 @@ const bcrypt = require('bcrypt')
 
 //register
 router.post("/register", async (req,res) => {
-  
-  
 
   try {
     //generate password
@@ -26,7 +24,22 @@ router.post("/register", async (req,res) => {
   } catch(err) {
     console.log(err)
   }
+});
 
+ //login
+router.post("/login", async (req, res) => {
+
+  try {
+    const user = await User.findOne({email: req.body.email});
+    !user && res.status(404).json("user not found");
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    !validPassword && res.status(400).json("wrong password");
+
+    res.status(200).json(user);
+  } catch(err) {
+    console.log(err);
+  }
 })
 
 module.exports = router
