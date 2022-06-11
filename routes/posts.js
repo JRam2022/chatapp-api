886d71ec921faf0c1887093299129096d5c7b062
JRam2022 <jasonram778@gmail.com>
2022-06-11 16:00:03 -0700

    Create update route

--- routes/posts.js
+++ routes/posts.js
@@ -13,7 +13,19 @@ router.post("/", async (req,res) => {
 })
 
 //update post
-
+router.put("/:id", async (req,res) => {
+  const post = await Post.findById(req.params.id);
+  try{
+    if(post.userId === req.body.userId) {
+      await post.updateOne({$set:req.body});
+      res.status(200).json("the post has been updated");
+    } else {
+      res.status(403).json("you can only update your post")
+    }
+  } catch(err) {
+    res.status(500).json(err)
+  }
+})
 
 //delete post
 //like post
