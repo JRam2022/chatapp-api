877a9ffba0c13369507605b47b9beb88b0267f5a
Brandon Felty <brcfelty@gmail.com>
2022-06-14 17:29:37 -0600

    adds find convo by id route

--- routes/conversations.js
+++ routes/conversations.js
@@ -15,4 +15,17 @@ router.post("/", async(req,res) => {
   }
 })
 
+//get user convos
+
+router.get("/:userId", async(req,res) => {
+  try{
+    const conversation = await Conversation.find({
+      members: {$in: [req.params.userId] },
+    });
+    res.status(200).json(conversation)
+  }catch(err) {
+    res.status(500).json(err)
+  }
+})
+
 module.exports = router;
