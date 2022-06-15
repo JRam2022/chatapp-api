ed7d947243703c3ea2f35374eb888dc7a924d72c
JRam2022 <jasonram778@gmail.com>
2022-06-14 20:02:18 -0700

    Install socket io server and create user functionality

--- index.js
+++ index.js
@@ -59,3 +59,43 @@ app.use("/api/messages", messageRoute);
 app.listen(8800, ()=> {
   console.log('Backend server is running!')
 })
+
+
+//Socket 
+const io = require("socket.io")(8900, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user)=>user.userId === userId) &&
+    users.push({ userId, socketId});
+}
+
+const removeUser = (socketId) => {
+  users = users.filter(user=>user.socketId !== socketId)
+}
+
+  
+io.on("connection", (socket) => {
+  //when connected
+  console.log("a user connected.");
+
+  // take userid and socket id from user
+  socket.on("addUser", userId=>{
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  })
+  
+  //send and get message
+
+  //when disconnected
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  })
+});
